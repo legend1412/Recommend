d58b094dfa75a9fa06e4a5434351f2a711c8bed4
legend1412 <zhaojianhao_1984@163.com>
2021-01-17 08:10:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,5 @@ import numpy as np
 str = " fff    "
 print(str.replace(' ', ''))
 arr = np.array([[180, 76], [158, 43], [176, 78], [161, 49]])
-print(arr ** 2)
-print((arr ** 2).sum(axis=1))
-
-print(-(1 / 4) * np.log2(3 / 4) - (1 / 12) * np.log2(1 / 4) - (2 / 3) * np.log2(1 / 2))
-print(-(5 / 12) * (3 / 5) * np.log2(3 / 5) - (5 / 12) * (2 / 5) * np.log2(2 / 5))
+print(arr**2)
+print((arr**2).sum(axis=1))
